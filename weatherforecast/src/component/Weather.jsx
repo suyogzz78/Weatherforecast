633d004cd28534803141c12a0ceecff7eedc42a0
Suyog Gautam <sakurachhetri97@gmail.com>
2026-01-24 22:48:15 +0545

    weather:updated the compo for better ui

--- weatherforecast/src/component/Weather.jsx
+++ weatherforecast/src/component/Weather.jsx
@@ -34,6 +34,11 @@ const Weather = () => {
 
   const search = async (city) => {
     try {
+
+      if(city == ""){
+        alert("Fillup the city name");
+        return ;
+      }
       const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_API_KEY}&units=metric`;
       const res = await fetch(url);
       const data = await res.json();
@@ -47,6 +52,7 @@ const Weather = () => {
         icon: icon,
       });
     } catch (error) {
+      setWeatherData(false);
       console.log("error");
     }
   };
@@ -55,62 +61,77 @@ const Weather = () => {
   }, []);
 
   return (
-    <div className="bg-violet-900 min-h-screen">
-      <h1 className="text-white text-3xl font-bold text-center p-10">
-        Weather Forecast
-      </h1>
+  <div className="bg-violet-900 min-h-screen">
+    <h1 className="text-white text-3xl font-bold text-center p-10">
+      Weather Forecast
+    </h1>
 
-      {/* Card */}
-      <div className="flex flex-col items-center gap-6 p-6 bg-gradient-to-r from-purple-700 to-purple-400 w-[400px] mx-auto rounded-md border-2 border-black">
-        {/* Search bar */}
-        <div className="flex flex-row items-center gap-3 w-full justify-center">
-          <input
-            ref={inputref}
-            type="text"
-            placeholder="Enter city name"
-            className="w-full p-2 rounded-full outline-none border-2 pl-4 text-center"
-          />
+    {/* Card */}
+    <div className="flex flex-col items-center gap-6 p-6 bg-gradient-to-r from-purple-700 to-purple-400 w-[400px] mx-auto rounded-md border-2 border-black">
+      
+      {/* Search bar */}
+      <div className="flex flex-row items-center gap-3 w-full justify-center">
+        <input
+          ref={inputref}
+          type="text"
+          placeholder="Enter city name"
+          className="w-full p-2 rounded-full outline-none border-2 pl-4 text-center"
+        />
 
-          <img
-            src={searchicon}
-            onClick={()=>{
-              search(inputref.current.value)
-            }}
-            alt="search"
-            className="h-9 w-9 bg-white rounded-full p-2 cursor-pointer"
-          />
-        </div>
-
-        {/* Weather Image */}
-        <div className="flex flex-col items-center">
-          <img src={weatherData.icon} alt="clear weather" className="w-32 h-32" />
-          <div>
-            <h2 className="text-white text-4xl font-semibold pl-4">{weatherData.temperature}°C</h2>
-            <p className="text-white text-lg mt-2 ">{weatherData.city}</p>
-          </div>
-        </div>
-        <div className="flex justify-between w-full px-10 ">
-          {/* Wind */}
-          <div className="flex flex-row items-center gap-3">
-            <img src={wind} className="w-10 h-10" />
-            <div>
-              <h2 className="text-white text-md font-semibold">{weatherData.windSpeed} km/h</h2>
-              <p className="text-white text-sm">Wind Speed</p>
-            </div>
-          </div>
-
-          {/* Humidity */}
-          <div className="flex flex-row items-center gap-3">
-            <img src={humidity} className="w-10 h-10" />
-            <div>
-              <h2 className="text-white text-md font-semibold">{weatherData.humidity}%</h2>
-              <p className="text-white text-sm">Humidity</p>
-            </div>
-          </div>
-        </div>
+        <img
+          src={searchicon}
+          onClick={() => search(inputref.current.value)}
+          alt="search"
+          className="h-9 w-9 bg-white rounded-full p-2 cursor-pointer"
+        />
       </div>
+
+      {weatherData && (
+        <>
+          <div className="flex flex-col items-center">
+            <img
+              src={weatherData.icon}
+              alt="weather"
+              className="w-32 h-32"
+            />
+
+            <h2 className="text-white text-4xl font-semibold">
+              {weatherData.temperature}°C
+            </h2>
+
+            <p className="text-white text-lg mt-2">
+              {weatherData.city}
+            </p>
+          </div>
+
+          <div className="flex justify-between w-full px-10">
+            {/* Wind */}
+            <div className="flex items-center gap-3">
+              <img src={wind} className="w-10 h-10" />
+              <div>
+                <h2 className="text-white text-md font-semibold">
+                  {weatherData.windSpeed} km/h
+                </h2>
+                <p className="text-white text-sm">Wind Speed</p>
+              </div>
+            </div>
+
+            {/* Humidity */}
+            <div className="flex items-center gap-3">
+              <img src={humidity} className="w-10 h-10" />
+              <div>
+                <h2 className="text-white text-md font-semibold">
+                  {weatherData.humidity}%
+                </h2>
+                <p className="text-white text-sm">Humidity</p>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
     </div>
-  );
-};
+  </div>
+)
+}
 
 export default Weather;
